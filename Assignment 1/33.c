a5f5a2755c28ab8d5e37a10e4eb4229de5479c37
Tanmay-Lalchandani <tanmaylalchandani1905@gmail.com>
2026-09-18 23:04:38 +0530

    Complete all questions

--- Assignment 1/33.c
+++ Assignment 1/33.c
@@ -1,30 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    int n, i, num;
-    int max, secondMax;
+    int n, i, num, max, min;
 
-    printf("Enter the value of n: ");
+    printf("Enter the value of N: ");
     scanf("%d", &n);
 
     printf("Enter %d numbers:\n", n);
 
     scanf("%d", &num);
     max = num;
-    secondMax = -2147483648; 
+    min = num;
 
     for (i = 2; i <= n; i++) {
         scanf("%d", &num);
         if (num > max) {
-            secondMax = max;
             max = num;
-        } else if (num > secondMax && num != max) {
-            secondMax = num;
+        }
+        if (num < min) {
+            min = num;
         }
     }
 
     printf("Max = %d\n", max);
-    printf("Second Max = %d\n", secondMax);
+    printf("Min = %d\n", min);
 
     return 0;
 }
---
+++ Assignment 1/36.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i = i + 2) {
+        printf("%d", i);
+        if (i + 2 <= n) {
+            printf(", ");
+        }
+    }
+    
+    return 0;
+}

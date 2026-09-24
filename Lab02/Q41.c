fc569af41ba6c74e669d4425261eed225243ae52
Tanmay-Lalchandani <tanmaylalchandani1905@gmail.com>
2026-09-24 15:22:12 +0530

    Initial commit Lab02

--- Lab02/Q41.c
+++ Lab02/Q41.c
@@ -4,12 +4,15 @@ int main() {
     int n,  i, factor;
     printf("Enter any positive number:\n");
     scanf("%d", &n);
-    for(i=1; i<=n; i++) {
+    printf("Factors of %d are:\n", n);
+
+    for(i=1; i<=n/2; i++) {
         if(n%i==0) {
             factor=i;
-        printf("Factors of %d are %d ", n, factor);
+        printf("%d ", factor);
         }
     
     }
+    printf("%d", n);
     return 0;
 }
